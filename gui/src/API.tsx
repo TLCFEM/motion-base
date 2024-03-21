dc589f0f7482f3814647d37a9368a549cf7ca780
Theodore Chang <tlcfem@gmail.com>
2024-03-21 05:19:10 +0100

    Minor update

--- gui/src/API.tsx
+++ gui/src/API.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://0.0.0.0:8000";
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
 axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
 
 export class SeismicRecord {
