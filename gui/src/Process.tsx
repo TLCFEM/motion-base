4e30bc4d43043e21fd83f67137527027d4d6064e
Theodore Chang <tlcfem@gmail.com>
2024-03-26 00:20:15 +0100

    Commit

--- gui/src/Process.tsx
+++ gui/src/Process.tsx
@@ -135,151 +135,192 @@ const Settings: Component<sxProps> = (props) => {
                 sx={{
                     ...props.sx,
                     display: "flex",
-                    flexDirection: "row",
+                    flexDirection: "column",
                     justifyContent: "center",
                     alignContent: "center",
-                    alignItems: "center",
-                    flexWrap: "wrap",
+                    alignItems: "flex-start",
                     gap: "1rem",
                 }}
             >
-                <TextField
-                    label="ID"
-                    value={currentRecord()}
-                    defaultValue={currentRecord()}
-                    onChange={(_, value) => setCurrentRecord(value)}
-                />
-                <FormControlLabel
-                    control={<Checkbox checked={withWaveform()} onChange={(_, checked) => setWithWaveform(checked)} />}
-                    label="Show Waveform"
-                />
-                <FormControlLabel
-                    control={<Checkbox checked={withSpectrum()} onChange={(_, checked) => setWithSpectrum(checked)} />}
-                    label="Compute Frequency Spectrum"
-                />
-                <FormControlLabel
-                    control={<Checkbox checked={withFilter()} onChange={(_, value) => setWithFilter(value)} />}
-                    label="Apply Filter"
-                />
-                <TextField
-                    label="Upsampling Ratio"
-                    type="number"
-                    value={ratio() > 0 ? ratio() : ""}
-                    defaultValue={ratio() > 0 ? ratio() : ""}
-                    onChange={(_, value) => setRatio(Math.max(0, Math.round(Number(value))))}
-                    disabled={!withFilter()}
-                />
-                <TextField
-                    label="Filter Length"
-                    type="number"
-                    value={filterLength() > 0 ? filterLength() : ""}
-                    defaultValue={filterLength() > 0 ? filterLength() : ""}
-                    onChange={(_, value) => setFilterLength(Math.max(0, Math.round(Number(value))))}
-                    disabled={!withFilter()}
-                />
-                <TextField
-                    error={ifError(lowCut())}
-                    label="Low Cut"
-                    type="number"
-                    value={lowCut()}
-                    defaultValue={lowCut()}
-                    onChange={(_, value) => setLowCut(value)}
-                    disabled={!withFilter() || filterType() === "lowpass"}
-                />
-                <TextField
-                    error={ifError(highCut())}
-                    label="High Cut"
-                    type="number"
-                    value={highCut()}
-                    defaultValue={highCut()}
-                    onChange={(_, value) => setHighCut(value)}
-                    disabled={!withFilter() || filterType() === "highpass"}
-                />
-                <FormControl sx={{ minWidth: "14ch" }} disabled={!withFilter()}>
-                    <InputLabel>Filter Type</InputLabel>
-                    <Select label="Filter Type" value={filterType()} onChange={(e) => setFilterType(e.target.value)}>
-                        <MenuItem value="lowpass">Lowpass</MenuItem>
-                        <MenuItem value="highpass">Highpass</MenuItem>
-                        <MenuItem value="bandpass">Bandpass</MenuItem>
-                    </Select>
-                </FormControl>
-                <FormControl sx={{ minWidth: "18ch" }} disabled={!withFilter()}>
-                    <InputLabel>Window Type</InputLabel>
-                    <Select label="Window Type" value={windowType()} onChange={(e) => setWindowType(e.target.value)}>
-                        <MenuItem value="flattop">FlatTop</MenuItem>
-                        <MenuItem value="blackmanharris">BlackmanHarris</MenuItem>
-                        <MenuItem value="nuttall">Nuttall</MenuItem>
-                        <MenuItem value="hann">Hann</MenuItem>
-                        <MenuItem value="hamming">Hamming</MenuItem>
-                        <MenuItem value="kaiser">Kaiser</MenuItem>
-                        <MenuItem value="chebwin">ChebWin</MenuItem>
-                    </Select>
-                </FormControl>
-                <FormControlLabel
-                    control={
-                        <Checkbox
-                            checked={withResponseSpectrum()}
-                            onChange={(_, checked) => {
-                                setWithResponseSpectrum(checked);
-                                resizePlot();
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignContent: "center",
+                        alignItems: "center",
+                        gap: "1rem",
+                    }}
+                >
+                    <FormControlLabel
+                        control={
+                            <Checkbox checked={withWaveform()} onChange={(_, checked) => setWithWaveform(checked)} />
+                        }
+                        label="Show Waveform"
+                    />
+                    <FormControlLabel
+                        control={
+                            <Checkbox checked={withSpectrum()} onChange={(_, checked) => setWithSpectrum(checked)} />
+                        }
+                        label="Compute Frequency Spectrum"
+                    />
+                    <TextField
+                        label="ID"
+                        value={currentRecord()}
+                        defaultValue={currentRecord()}
+                        onChange={(_, value) => setCurrentRecord(value)}
+                    />
+                    <ButtonGroup variant="outlined" orientation="horizontal">
+                        <Button onClick={process} id="btn-process" disabled={loading()}>
+                            Process
+                        </Button>
+                        <Button onClick={clear} id="btn-reset" disabled={loading()}>
+                            Reset
+                        </Button>
+                        <Button onClick={download} id="btn-download" disabled={loading()}>
+                            Download
+                        </Button>
+                    </ButtonGroup>
+                    <Modal open={error() !== ""} onClose={() => setError("")}>
+                        <Alert
+                            severity="error"
+                            sx={{
+                                position: "absolute",
+                                top: "50%",
+                                left: "50%",
+                                transform: "translate(-50%, -50%)",
+                                width: "40%",
                             }}
-                        />
-                    }
-                    label="Compute Response Spectrum"
-                />
-                <TextField
-                    error={ifError(dampingRatio())}
-                    label="Damping Ratio"
-                    type="number"
-                    value={dampingRatio()}
-                    defaultValue={dampingRatio()}
-                    onChange={(_, value) => setDampingRatio(value)}
-                    disabled={!withResponseSpectrum()}
-                />
-                <TextField
-                    error={ifError(periodStep())}
-                    label="Period Step"
-                    type="number"
-                    value={periodStep()}
-                    defaultValue={periodStep()}
-                    onChange={(_, value) => setPeriodStep(value)}
-                    disabled={!withResponseSpectrum()}
-                />
-                <TextField
-                    error={ifError(periodEnd())}
-                    label="Period End"
-                    type="number"
-                    value={periodEnd()}
-                    defaultValue={periodEnd()}
-                    onChange={(_, value) => setPeriodEnd(value)}
-                    disabled={!withResponseSpectrum()}
-                />
-                <ButtonGroup variant="outlined" orientation="horizontal">
-                    <Button onClick={process} id="btn-process" disabled={loading()}>
-                        Process
-                    </Button>
-                    <Button onClick={clear} id="btn-reset" disabled={loading()}>
-                        Reset
-                    </Button>
-                    <Button onClick={download} id="btn-download" disabled={loading()}>
-                        Download
-                    </Button>
-                </ButtonGroup>
-                <Modal open={error() !== ""} onClose={() => setError("")}>
-                    <Alert
-                        severity="error"
-                        sx={{
-                            position: "absolute",
-                            top: "50%",
-                            left: "50%",
-                            transform: "translate(-50%, -50%)",
-                            width: "40%",
-                        }}
-                    >
-                        <AlertTitle>Error</AlertTitle>
-                        {error()}
-                    </Alert>
-                </Modal>
+                        >
+                            <AlertTitle>Error</AlertTitle>
+                            {error()}
+                        </Alert>
+                    </Modal>
+                </Box>
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignContent: "center",
+                        alignItems: "center",
+                        gap: "1rem",
+                    }}
+                >
+                    <FormControlLabel
+                        control={<Checkbox checked={withFilter()} onChange={(_, value) => setWithFilter(value)} />}
+                        label="Apply Filter"
+                    />
+                    <TextField
+                        label="Upsampling Ratio"
+                        type="number"
+                        value={ratio() > 0 ? ratio() : ""}
+                        defaultValue={ratio() > 0 ? ratio() : ""}
+                        onChange={(_, value) => setRatio(Math.max(0, Math.round(Number(value))))}
+                        disabled={!withFilter()}
+                    />
+                    <TextField
+                        label="Filter Length"
+                        type="number"
+                        value={filterLength() > 0 ? filterLength() : ""}
+                        defaultValue={filterLength() > 0 ? filterLength() : ""}
+                        onChange={(_, value) => setFilterLength(Math.max(0, Math.round(Number(value))))}
+                        disabled={!withFilter()}
+                    />
+                    <TextField
+                        error={ifError(lowCut())}
+                        label="Low Cut"
+                        type="number"
+                        value={lowCut()}
+                        defaultValue={lowCut()}
+                        onChange={(_, value) => setLowCut(value)}
+                        disabled={!withFilter() || filterType() === "lowpass"}
+                    />
+                    <TextField
+                        error={ifError(highCut())}
+                        label="High Cut"
+                        type="number"
+                        value={highCut()}
+                        defaultValue={highCut()}
+                        onChange={(_, value) => setHighCut(value)}
+                        disabled={!withFilter() || filterType() === "highpass"}
+                    />
+                    <FormControl sx={{ minWidth: "14ch" }} disabled={!withFilter()}>
+                        <InputLabel>Filter Type</InputLabel>
+                        <Select
+                            label="Filter Type"
+                            value={filterType()}
+                            onChange={(e) => setFilterType(e.target.value)}
+                        >
+                            <MenuItem value="lowpass">Lowpass</MenuItem>
+                            <MenuItem value="highpass">Highpass</MenuItem>
+                            <MenuItem value="bandpass">Bandpass</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <FormControl sx={{ minWidth: "18ch" }} disabled={!withFilter()}>
+                        <InputLabel>Window Type</InputLabel>
+                        <Select
+                            label="Window Type"
+                            value={windowType()}
+                            onChange={(e) => setWindowType(e.target.value)}
+                        >
+                            <MenuItem value="flattop">FlatTop</MenuItem>
+                            <MenuItem value="blackmanharris">BlackmanHarris</MenuItem>
+                            <MenuItem value="nuttall">Nuttall</MenuItem>
+                            <MenuItem value="hann">Hann</MenuItem>
+                            <MenuItem value="hamming">Hamming</MenuItem>
+                            <MenuItem value="kaiser">Kaiser</MenuItem>
+                            <MenuItem value="chebwin">ChebWin</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Box>
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignContent: "center",
+                        alignItems: "center",
+                        gap: "1rem",
+                    }}
+                >
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                checked={withResponseSpectrum()}
+                                onChange={(_, checked) => {
+                                    setWithResponseSpectrum(checked);
+                                    resizePlot();
+                                }}
+                            />
+                        }
+                        label="Compute Response Spectrum"
+                    />
+                    <TextField
+                        error={ifError(dampingRatio())}
+                        label="Damping Ratio"
+                        type="number"
+                        value={dampingRatio()}
+                        defaultValue={dampingRatio()}
+                        onChange={(_, value) => setDampingRatio(value)}
+                        disabled={!withResponseSpectrum()}
+                    />
+                    <TextField
+                        error={ifError(periodStep())}
+                        label="Period Step"
+                        type="number"
+                        value={periodStep()}
+                        defaultValue={periodStep()}
+                        onChange={(_, value) => setPeriodStep(value)}
+                        disabled={!withResponseSpectrum()}
+                    />
+                    <TextField
+                        error={ifError(periodEnd())}
+                        label="Period End"
+                        type="number"
+                        value={periodEnd()}
+                        defaultValue={periodEnd()}
+                        onChange={(_, value) => setPeriodEnd(value)}
+                        disabled={!withResponseSpectrum()}
+                    />
+                </Box>
             </CardContent>
             <Box sx={{ width: "100%" }}>
                 {loading() ? <LinearProgress /> : <LinearProgress variant="determinate" value={0} />}
