7bb0a6a3a9430105e2d1da1e9b51fd888c1ee815
Theodore Chang <tlcfem@gmail.com>
2024-03-27 01:41:15 +0100

    Commit

--- src/mb/utility/config.py
+++ src/mb/utility/config.py
@@ -47,7 +47,10 @@ def mongo_uri():
 async def init_mongo():
     uri = mongo_uri()
     connect(host=f"{uri}StrongMotion?authSource=admin", uuidrepresentation="standard")
-    await init_beanie(database=AsyncIOMotorClient(uri)["StrongMotion"], document_models=[Record, User, UploadTask])
+    await init_beanie(
+        database=AsyncIOMotorClient(uri, uuidRepresentation="standard")["StrongMotion"],
+        document_models=[Record, User, UploadTask],
+    )
 
 
 async def shutdown_mongo():
