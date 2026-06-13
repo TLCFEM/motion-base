69ec533fabc24f1bafef9b2d80a2e36f83368b6b
Theodore Chang <tlcfem@gmail.com>
2026-06-13 14:36:39 +0200

    Refactor test cases for JP and NZ file parsing; enhance assertions and parameterization

--- tests/record/test_jp_sync.py
+++ tests/record/test_jp_sync.py
@@ -13,7 +13,6 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import os
 
 import pytest
 
@@ -24,7 +23,33 @@ from mb.utility import UPath
 
 @pytest.mark.parametrize("file_path", ["data/SZO0039901271027.NS"])
 async def test_jp_parse_file(pwd, file_path, mongo_connection):
-    await ParserNIED.parse_file(os.path.join(pwd, file_path))
+    result = await ParserNIED.parse_file((UPath(pwd) / file_path).as_posix())
+    assert result.model_dump(
+        mode="json",
+        exclude_none=True,
+        exclude_defaults=True,
+        exclude={"raw_data", "id", "uploaded_by"},
+    ) == {
+        "file_hash": "f738850f18d5d1494c0059b8c784b0b9bce347db3e217be23379ad7c4db4f8d1",
+        "region": "jp",
+        "magnitude": 3.6,
+        "maximum_acceleration": 25.836,
+        "event_time": "1999-01-27T10:27:00+09:00",
+        "event_location": [139.647, 34.82],
+        "depth": 38.0,
+        "station_code": "SZO003",
+        "station_location": [139.0544, 34.8158],
+        "station_elevation": 149.0,
+        "station_elevation_unit": "meter",
+        "record_time": "1999-01-27T10:27:55+09:00",
+        "last_update_time": "1999-01-27T10:00:00+09:00",
+        "sampling_frequency": 100.0,
+        "sampling_frequency_unit": "hertz",
+        "duration": 119.0,
+        "direction": "NS",
+        "scale_factor": 0.0002384185791015625,
+        "raw_data_unit": "galileo",
+    }
 
 
 @pytest.mark.parametrize("file_path", ["data/jp_test.knt.tar.gz"])
